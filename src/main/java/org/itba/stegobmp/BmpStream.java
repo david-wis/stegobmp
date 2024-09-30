b5e49d5fadd3958e118ad738e626c4f1e61249d0
david-wis <dwischnevsky@itba.edu.ar>
2024-09-30 12:52:42 -0300

    LSB1 (without encryption)

--- src/main/java/org/itba/stegobmp/BmpStream.java
+++ src/main/java/org/itba/stegobmp/BmpStream.java
@@ -1,47 +1,43 @@
 package org.itba.stegobmp;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
-public class BmpStream extends FileStream {
+public class BmpStream extends FileInputStream {
+    private byte[] header = new byte[54];
+    private int fileSize;
 
+    public BmpStream(String bmpFilePath) throws IOException {
+        super(getFileFromResources(bmpFilePath));
 
-    private FileStatus status = FileStatus.STARTED;
-    public BmpStream(String bmpFilePath, int bufferSize) throws IOException {
-        super(bmpFilePath, bufferSize);
-
-        byte[] buffer = new byte[54];
-        int bytesRead = inputStream.read(buffer);
+        int bytesRead = this.read(header);
         if (bytesRead == -1) {
-            throw new RuntimeException("End of file reached: " + filePath); // TODO: Custom exception?
+            throw new RuntimeException("End of file reached: " + bmpFilePath); // TODO: Custom exception?
         }
 
-        if (buffer[0] != 'B' || buffer[1] != 'M' || buffer[28] != 24 || buffer[30] != 0 || buffer[31] != 0) {
-            throw new RuntimeException("Invalid BMP file: " + filePath); // TODO: Custom exception?
+        if (header[0] != 'B' || header[1] != 'M' || header[28] != 24 || header[30] != 0 || header[31] != 0) {
+            throw new RuntimeException("Invalid BMP file: " + bmpFilePath); // TODO: Custom exception?
         }
 
-        this.fileSize =  ((buffer[5] & 0xFF) << 24) | ((buffer[4] & 0xFF) << 16) | ((buffer[3] & 0xFF) << 8) | (buffer[2] & 0xFF);
+        this.fileSize =  ((header[5] & 0xFF) << 24) | ((header[4] & 0xFF) << 16) | ((header[3] & 0xFF) << 8) | (header[2] & 0xFF);
     }
 
+    private static String getFileFromResources(String fileName) throws IOException {
+        ClassLoader classLoader = BmpStream.class.getClassLoader();
 
-    private enum FileStatus {
-        STARTED,
-        READING,
-        EXTENSION,
-        FINISHED
-    }
-
-    @Override
-    public boolean hasNext() {
-        return super.hasNext();
-    }
-
-    @Override
-    public byte[] next() {
-        if (status == FileStatus.STARTED) {
-            status = FileStatus.READING;
-            start = 4;
-//            buffer
+        if (classLoader.getResource(fileName) != null) {
+            return classLoader.getResource(fileName).getPath();
+        } else {
+            throw new IOException("File not found in resources: " + fileName);
         }
-        return super.next();
+    }
+
+    public byte[] getHeader() {
+        return header;
+    }
+
+    public int getFileSize() {
+        return fileSize;
     }
 }
--- src/main/java/org/itba/stegobmp/Embedders/LSB.java
+++ src/main/java/org/itba/stegobmp/Embedders/LSB.java
@@ -1,11 +1,9 @@
 package org.itba.stegobmp.Embedders;
 
 import org.itba.stegobmp.BmpStream;
-import org.itba.stegobmp.FileStream;
 
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.Iterator;
 
 public class LSB {
     public LSB() {
@@ -13,41 +11,89 @@ public class LSB {
 
 
     public static void embed(String inputFile, String carrierFile, String outputFile) throws IOException {
-        FileStream input = new FileStream(inputFile, 512);
-        BmpStream carrier = new BmpStream(carrierFile, 4096);
+        InputStream input = LSB.class.getClassLoader().getResourceAsStream(inputFile);
 
-        BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(outputFile));
+        BmpStream carrier = new BmpStream(carrierFile);
+        OutputStream output = new FileOutputStream(outputFile);
 
-        while (carrier.hasNext()) {
-            byte[] secretBytes = (input.hasNext())? input.next() : new byte[0];
-            byte[] inputBytes = carrier.next();
-            byte[] outputBytes = new byte[inputBytes.length];
-            for (int i = 0; i < inputBytes.length; i++) {
-                if (i < secretBytes.length * 8) {
-                    outputBytes[i] = (byte) ((inputBytes[i] & 0xFE) | ((secretBytes[i / 8] >> (7 - i % 8)) & 1));
-                } else {
-                    outputBytes[i] = inputBytes[i];
-                }
-            }
-            output.write(outputBytes);
+        byte[] inputBytes = input.readAllBytes();
+        int inputLen = inputBytes.length;
+        // convert to hex for the bytes
+        byte[] inputLenBytes = new byte[4];
+
+        // byte[] inputLenBytes = new byte[4];
+        inputLenBytes[0] = (byte) (inputLen >> 24);
+        inputLenBytes[1] = (byte) (inputLen >> 16);
+        inputLenBytes[2] = (byte) (inputLen >> 8);
+        inputLenBytes[3] = (byte) inputLen;
+
+        String fileExtension = inputFile.substring(inputFile.lastIndexOf('.'));
+        byte[] fileExtensionBytes = fileExtension.getBytes(); // TODO: Check that the \0 is included
+
+        byte[] carrierBytes = carrier.readAllBytes();
+
+        if (inputLen + 4 + fileExtensionBytes.length > carrierBytes.length/8) {
+            throw new RuntimeException("Input message is too long for the carrier");
         }
+
+        // the input message will be length || message || extension
+        byte[] inputMessage = new byte[4 + inputLen + fileExtensionBytes.length];
+        System.arraycopy(inputLenBytes, 0, inputMessage, 0, 4);
+        System.arraycopy(inputBytes, 0, inputMessage, 4, inputLen);
+        System.arraycopy(fileExtensionBytes, 0, inputMessage, 4 + inputLen, fileExtensionBytes.length);
+
+        output.write(carrier.getHeader(), 0, 54);
+
+        Iterator<Integer> bitIterator = new Iterator<>() {
+            int pos = 0;
+            @Override
+            public boolean hasNext() {
+                return pos < inputMessage.length * 8;
+            }
+
+            @Override
+            public Integer next() {
+                Integer bit = ((inputMessage[pos / 8] >> (7 - pos % 8)) & 1);
+                pos++;
+                return bit;
+            }
+        };
+
+        for (int i = 0; i < carrierBytes.length; i++) {
+            if (bitIterator.hasNext()) {
+                carrierBytes[i] = (byte) ((carrierBytes[i] & 0xFE) | bitIterator.next());
+            }
+        }
+        output.write(carrierBytes);
+
+        input.close();
+        carrier.close();
+        output.close();
     }
 
     public static void extract(String bitmapFile, String outputFile) throws IOException {
-        FileStream bitmap = new FileStream(bitmapFile, 4096);
-        BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(outputFile));
+        BmpStream bmpStream = new BmpStream(bitmapFile);
+        OutputStream output = new FileOutputStream(outputFile);
 
-        while (bitmap.hasNext()) {
-            byte[] inputBytes = bitmap.next();
-            byte[] outputBytes = new byte[inputBytes.length / 8];
-            for (int i = 0; i < outputBytes.length; i++) {
-                outputBytes[i] = 0;
-                for (int j = 0; j < 8; j++) {
-                    outputBytes[i] |= (byte) ((inputBytes[i * 8 + j] & 1) << (7 - j));
-                }
-            }
-            output.write(outputBytes);
+        byte[] bmpBytes = bmpStream.readAllBytes();
+        byte[] inputLenBytes = new byte[4];
+        inputLenBytes[0] = (byte) ((bmpBytes[0] & 1) << 7 | (bmpBytes[1] & 1) << 6 | (bmpBytes[2] & 1) << 5 | (bmpBytes[3] & 1) << 4 | (bmpBytes[4] & 1) << 3 | (bmpBytes[5] & 1) << 2 | (bmpBytes[6] & 1) << 1 | (bmpBytes[7] & 1));
+        inputLenBytes[1] = (byte) ((bmpBytes[8] & 1) << 7 | (bmpBytes[9] & 1) << 6 | (bmpBytes[10] & 1) << 5 | (bmpBytes[11] & 1) << 4 | (bmpBytes[12] & 1) << 3 | (bmpBytes[13] & 1) << 2 | (bmpBytes[14] & 1) << 1 | (bmpBytes[15] & 1));
+        inputLenBytes[2] = (byte) ((bmpBytes[16] & 1) << 7 | (bmpBytes[17] & 1) << 6 | (bmpBytes[18] & 1) << 5 | (bmpBytes[19] & 1) << 4 | (bmpBytes[20] & 1) << 3 | (bmpBytes[21] & 1) << 2 | (bmpBytes[22] & 1) << 1 | (bmpBytes[23] & 1));
+        inputLenBytes[3] = (byte) ((bmpBytes[24] & 1) << 7 | (bmpBytes[25] & 1) << 6 | (bmpBytes[26] & 1) << 5 | (bmpBytes[27] & 1) << 4 | (bmpBytes[28] & 1) << 3 | (bmpBytes[29] & 1) << 2 | (bmpBytes[30] & 1) << 1 | (bmpBytes[31] & 1));
+
+
+        int inputLen = ((inputLenBytes[0] & 0xFF) << 24) | ((inputLenBytes[1] & 0xFF) << 16) | ((inputLenBytes[2] & 0xFF) << 8) | (inputLenBytes[3] & 0xFF);
+
+        byte[] inputMessage = new byte[inputLen];
+        for (int i = 0; i < inputLen; i++) {
+            inputMessage[i] = (byte) ((bmpBytes[32 + i * 8] & 1) << 7 | (bmpBytes[33 + i * 8] & 1) << 6 | (bmpBytes[34 + i * 8] & 1) << 5 | (bmpBytes[35 + i * 8] & 1) << 4 | (bmpBytes[36 + i * 8] & 1) << 3 | (bmpBytes[37 + i * 8] & 1) << 2 | (bmpBytes[38 + i * 8] & 1) << 1 | (bmpBytes[39 + i * 8] & 1));
         }
+
+        output.write(inputMessage);
+
+        output.close();
+        bmpStream.close();
     }
 
 
