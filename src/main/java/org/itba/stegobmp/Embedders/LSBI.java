a9809383b315dab3add170cf2ff0c19379314a01
davidwis <davidwis000@gmail.com>
2024-10-11 12:17:35 -0300

    LSBI extract fixed

--- src/main/java/org/itba/stegobmp/Embedders/LSBI.java
+++ src/main/java/org/itba/stegobmp/Embedders/LSBI.java
@@ -6,8 +6,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.function.Predicate;
 
 public class LSBI implements StegoAlgorithm {
+
+    private static final int LENGTH_LENGTH = 32;
+
     @Override
     public void embedInFile(String inputFile, String carrierFile, String outputFile) throws IOException {
         InputStream input = LSB.class.getClassLoader().getResourceAsStream(inputFile);
@@ -28,12 +32,13 @@ public class LSBI implements StegoAlgorithm {
         OutputStream output = new FileOutputStream(outputFile);
         output.write(carrier.getHeader(), 0, 54);
         for (int i = 0; i < stats.length; i++) {
-             output.write((firstBytes[i] & 0xFE) | ((stats[i] > 0) ? 0x1 : 0x0));
+            output.write((firstBytes[i] & 0xFE) | ((stats[i] > 0) ? 0x1 : 0x0));
         }
         for (int i = 0; i < lsb1Result.length; i++) {
-            if (i == 538799+58)
-                System.out.println("Here");
-            if (i % 3 == 1 || stats[(lsb1Result[i] & 0b00000110) >> 1] <= 0) continue; // Skip red channels, or unchanged bytes
+//            if (i == 538799+58)
+//                System.out.println("Here");
+            if (i % 3 == 1 || stats[(lsb1Result[i] & 0b00000110) >> 1] <= 0)
+                continue; // Skip red channels, or unchanged bytes
             lsb1Result[i] ^= 0x1;
         }
 
@@ -43,12 +48,65 @@ public class LSBI implements StegoAlgorithm {
         carrier.close();
     }
 
+    private void readBytes(byte[] bmpBytes, int limit, int offset, byte[] writeBuffer, int[] flipBits) {
+        readBytes(bmpBytes, i -> i < limit, offset, writeBuffer, flipBits);
+    }
+
+    private int readBytes(byte[] bmpBytes, Predicate<Integer> loopCondition, int offset, byte[] writeBuffer, int[] flipBits) {
+        int i;
+        for (i = 0; loopCondition.test(i); i++) {
+            int b = 0x0;
+            for (int j = 0; j < 8; j++) {
+                if ((offset + i * 8 + j) % 3 == 2) { // This is absolute!!!!
+                    j--;
+                    offset++;
+                    continue;
+                }
+                int group = (bmpBytes[offset + i * 8 + j] & 0b00000110) >> 1;
+                b |= ((bmpBytes[offset + i * 8 + j] & 0x1) ^ flipBits[group]) << (7 - j);
+            }
+            writeBuffer[i] = (byte) b;
+        }
+        return i;
+    }
+
+    private static int skipRed(int writtenLength, int offset) {
+        return writtenLength+ (writtenLength + offset % 3) / 2;
+    }
+
     @Override
     public void extract(String bitmapFile, String outputFile) throws IOException {
         BmpStream bmpStream = new BmpStream(bitmapFile);
 
+
         byte[] bmpBytes = bmpStream.readAllBytes();
+
+        int[] flipBits = new int[4];
+        for (int i = 0; i < 4; i++)
+            flipBits[i] = (bmpBytes[i] & 0x1) == 0 ? 0x0 : 0x1;
+
+        int offset = 4;
         byte[] inputLenBytes = new byte[4];
+        readBytes(bmpBytes, 4, offset, inputLenBytes, flipBits);
+
+        int inputLen = ((inputLenBytes[0] & 0xFF) << 24) | ((inputLenBytes[1] & 0xFF) << 16) | ((inputLenBytes[2] & 0xFF) << 8) | (inputLenBytes[3] & 0xFF);
+        System.out.println("inputLen " + inputLen);
+        byte[] inputMessage = new byte[inputLen];
+        offset += skipRed(LENGTH_LENGTH, offset);
+        readBytes(bmpBytes, inputLen, offset, inputMessage, flipBits);
+        // B G R B | G -R B G -R B G -R B G -R B G -R B G -R B G -R B G -R B
+
+        byte[] extension = new byte[255];
+        offset += skipRed(inputLen * 8, offset);
+        int extensionLen = readBytes(bmpBytes, i -> i == 0 || extension[i-1] != 0, offset, extension, flipBits) - 1;
+
+        String extensionStr = new String(extension, 0, extensionLen);
+        System.out.println(outputFile + extensionStr);
+        OutputStream output = new FileOutputStream(outputFile + extensionStr);
+        output.write(inputMessage);
+        output.close();
+        bmpStream.close();
+
 
     }
 }
